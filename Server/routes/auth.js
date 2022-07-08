1e1f3a4a90c4c96a1eab071ccb2f5cc383fe2fc6
Nitish8800 <nitishfea120784@gmail.com>
2022-07-09 02:53:23 +0530

    posting data in mongo

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-// const mongoose = require('mongoose')
-// const User = mongoose.model("User")
-// const crypto = require('crypto')
-// const bcrypt = require('bcryptjs')
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const crypto = require("crypto");
+const bcrypt = require("bcryptjs");
 // const jwt = require('jsonwebtoken')
 // const {JWT_SECRET} = require('../config/keys')
 // const requireLogin = require('../middleware/requireLogin')
@@ -23,45 +23,45 @@ router.get("/", (req, res) => {
 // }))
 
 router.post("/signup", (req, res) => {
-//   console.log(req.body);
-  //   const { name, email, password, pic } = req.body;
-  //   if (!email || !password || !name) {
-  //     return res.status(422).json({ error: "please add all the fields" });
-  //   }
-  //   User.findOne({ email: email })
-  //     .then((savedUser) => {
-  //       if (savedUser) {
-  //         return res
-  //           .status(422)
-  //           .json({ error: "user already exists with that email" });
-  //       }
-  //       bcrypt.hash(password, 12).then((hashedpassword) => {
-  //         const user = new User({
-  //           email,
-  //           password: hashedpassword,
-  //           name,
-  //           pic,
-  //         });
+  //   console.log(req.body);
+  const { name, email, password, pic } = req.body;
+  if (!email || !password || !name) {
+    return res.status(422).json({ error: "please fill all the fields" });
+  }
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res
+          .status(422)
+          .json({ error: "user already exists with that email" });
+      }
+      bcrypt.hash(password, 12).then((hashedpassword) => {
+        const user = new User({
+          email,
+          password: hashedpassword,
+          name,
+          pic,
+        });
 
-  //         user
-  //           .save()
-  //           .then((user) => {
-  //             // transporter.sendMail({
-  //             //     to:user.email,
-  //             //     from:"no-reply@insta.com",
-  //             //     subject:"signup success",
-  //             //     html:"<h1>welcome to instagram</h1>"
-  //             // })
-  //             res.json({ message: "saved successfully" });
-  //           })
-  //           .catch((err) => {
-  //             console.log(err);
-  //           });
-  //       });
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
+        user
+          .save()
+          .then((user) => {
+            // transporter.sendMail({
+            //     to:user.email,
+            //     from:"no-reply@insta.com",
+            //     subject:"signup success",
+            //     html:"<h1>welcome to instagram</h1>"
+            // })
+            res.json({ message: "saved successfully" });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 // router.post('/signin',(req,res)=>{
