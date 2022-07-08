55971dd66b67dd77b8d748ba2579699a43c7d67e
Nitish8800 <nitishfea120784@gmail.com>
2022-07-09 02:24:27 +0530

    getting data in request body

--- Server/app.js
+++ Server/app.js
@@ -2,29 +2,24 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
-// const { MONGOURI } = require("./config/keys");
+const { MONGOURI } = require("./config/keys");
 
-const customMiddleware = (req, res, next) => {
-  console.log("Middleware Executed");
-  next();
-};
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("conneted to mongodb yeahh");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("err connecting", err);
+});
 
-// mongoose.connect(MONGOURI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-// mongoose.connection.on("connected", () => {
-//   console.log("conneted to mongo yeahh");
-// });
-// mongoose.connection.on("error", (err) => {
-//   console.log("err connecting", err);
-// });
+require("./models/user");
+require("./models/post");
 
-// require("./models/user");
-// require("./models/post");
-
-// app.use(express.json());
-// app.use(require("./routes/auth"));
+app.use(express.json());
+app.use(require("./routes/auth"));
 // app.use(require("./routes/post"));
 // app.use(require("./routes/user"));
 
@@ -39,20 +34,3 @@ const customMiddleware = (req, res, next) => {
 app.listen(PORT, () => {
   console.log("server is running on", PORT);
 });
-
-/* // <<<<<<< Middleware >>>>>>>>
-
-// app.use(customMiddleware);  // for multiple route
-
-app.get("/", (req, res) => {
-  console.log("Home");
-  res.send("Hello World");
-});
-
-app.get("/about", customMiddleware, (req, res) => {
-  // for single route use middleware in between bracket. its prebuild middleware
-  console.log("About");
-  res.send("About page");
-});
-
-*/
---
+++ Server/config/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI:"mongodb+srv://Niku800:8800@my-social-media.kob4h.mongodb.net/My_Social_Media"
+}
